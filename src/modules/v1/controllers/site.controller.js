6488c53b84807ed14ed882e359420f71d5a5f1bd
haipc <haiham1999.90@gmail.com>
2022-09-01 23:03:18 +0700

    TEST|webhook: log webhook

--- src/modules/v1/controllers/site.controller.js
+++ src/modules/v1/controllers/site.controller.js
@@ -32,6 +32,7 @@ class SiteController {
   }
   // [GET] /test
   async handleWebhook(req, res, next) {
+    console.log("webhook: ", req.body)
     res.status(200).json({
       status: "SUCCESS",
       message: "webhook test",
